639a4974a61b5e7622e714c6bc440d60c8e90902
vboxuser <vboxuser@localhost.localdomain>
2026-08-11 18:18:36 +0000

    Ajout du composant Login

--- frontend/stepup-client/src/App.jsx
+++ frontend/stepup-client/src/App.jsx
@@ -1,13 +1,21 @@
-import Navbar from "./components/layout/Navbar"
-import Footer from "./components/layout/Footer"
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Login from "./pages/Login.jsx";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-1"></main>
+
+      <main className="flex-1">
+        <Routes>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+      </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
